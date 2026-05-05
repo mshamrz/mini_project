5a0233577fbcd044c5a22db76474b0c83414499c
Мороз Мар'яна <m.moroz.pn@ucu.edu.ua>
2026-05-05 12:23:50 +0300

    Add solution to 73_Set_Matrix_Zeroes.py

---
+++ module_5_Math_and_Geometry/73_Set_Matrix_Zeroes.py
@@ -0,0 +1,23 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        first_row = any(matrix[0][j] == 0 for j in range(len(matrix[0])))
+        first_col = any(matrix[i][0] == 0 for i in range(len(matrix)))
+        for i in range(1, len(matrix)):
+            for j in range(1, len(matrix[0])):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+        for i in range(1, len(matrix)):
+            for j in range(1, len(matrix[0])):
+                if matrix[0][j] == 0 or matrix[i][0] == 0:
+                    matrix[i][j] = 0
+
+        if first_row:
+            for j in range(len(matrix[0])):
+                matrix[0][j] = 0
+        if first_col:
+            for i in range(len(matrix)):
+                matrix[i][0] = 0
