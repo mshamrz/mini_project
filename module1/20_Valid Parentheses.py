46f4fa48525fefb2a7b19cb5e112f7a9a9f4e4ad
mshamrz <m.moroz.pn@ucu.edu.ua>
2026-05-05 11:55:36 +0300

    Create solution 20_Valid Parentheses.py
    module about stack

---
+++ module1/20_Valid Parentheses.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        mapping = {')': '(', ']': '[', '}': '{'}
+        for char in s:
+            if char in '([{':
+                stack.append(char)
+            else:
+                if not stack or stack.pop() != mapping[char]:
+                    return False
+        return len(stack) == 0
