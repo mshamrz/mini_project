4403216f587dfdcbcba503108d232eebdfd142fe
Мороз Мар'яна <m.moroz.pn@ucu.edu.ua>
2026-05-06 17:16:21 +0300

    add solution for Subtree of Another Tree

---
+++ module_9_Binary_Tree/52_Subtree_of_Another_Tree.py
@@ -0,0 +1,25 @@
+from typing import Optional
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        def is_same(t1, t2):
+            if not t1 and not t2:
+                return True
+            if not t1 or not t2:
+                return False
+            if t1.val != t2.val:
+                return False
+            return (is_same(t1.left, t2.left) and
+                    is_same(t1.right, t2.right))
+        def dfs(node):
+            if not node:
+                return False
+            if is_same(node, subRoot):
+                return True
+            return dfs(node.left) or dfs(node.right)
+        return dfs(root)
